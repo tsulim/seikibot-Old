afafdb6f0debed82d31f533ebfde65067d8e1c31
XJLim <limxiujia18@gmail.com>
2020-06-09 15:09:26 +0800

    Edited help cmd - Added info on other cmd

--- commands/help.js
+++ commands/help.js
@@ -2,24 +2,62 @@ module.exports = {
     name: 'help',
     description: 'Show all commands',
     execute(msg, args) {
-        // msg.channel.send("m - minutes, s - seconds, d - days.\n\t{@User}");
-        msg.channel.send({embed: {
-            color: 0x0080ff,
-            // author: {
-            //     name: "Tsu",
-            //     icon_url: "https://cdn.discordapp.com/avatars/516303284873461760/e1a7397683aed74e04b46e2be2c03913.png?size=1024",
-            // },
-            title: "Command List",
-            description: "Still working on the bot but here are the working and available commands for you :D",
-            fields: [{
-                name: "Available Commands",
-                value: "`help` `create`",
-            }],
-            timestamp: new Date(),
-            footer: {
-                icon_url: "https://cdn.discordapp.com/avatars/516303284873461760/e1a7397683aed74e04b46e2be2c03913.png?size=1024",
-                text: "© Example",
-            }            
-        }});
+        if (args[0] == "create") {
+            msg.channel.send({embed: {
+                color: 0x0080ff,
+                title: "Help",
+                description: "Create a reminder for your event",
+                fields: [{
+                    name: "Usage:",
+                    value: "`-create <event name> <eventDate> <eventTime>`",
+                    },
+                    {
+                    name: "Example:",
+                    value: "`-create Example event name 2020-09-23 14:00`",
+                    }
+                ],
+                timestamp: new Date(),
+                footer: {
+                    icon_url: "https://cdn.discordapp.com/avatars/516303284873461760/e1a7397683aed74e04b46e2be2c03913.png?size=1024",
+                    text: "© Example",
+                }            
+            }});
+        } else if (args[0] == "listevent") {
+            msg.channel.send({embed: {
+                color: 0x0080ff,
+                title: "Help",
+                description: "List you down all the ongoing events in your server",
+                fields: [{
+                    name: "Usage:",
+                    value: "`-listevent`",
+                    },
+                ],
+                timestamp: new Date(),
+                footer: {
+                    icon_url: "https://cdn.discordapp.com/avatars/516303284873461760/e1a7397683aed74e04b46e2be2c03913.png?size=1024",
+                    text: "© Example",
+                }            
+            }});
+        } else {
+            // msg.channel.send("m - minutes, s - seconds, d - days.\n\t{@User}");
+            msg.channel.send({embed: {
+                color: 0x0080ff,
+                // author: {
+                //     name: "Tsu",
+                //     icon_url: "https://cdn.discordapp.com/avatars/516303284873461760/e1a7397683aed74e04b46e2be2c03913.png?size=1024",
+                // },
+                title: "Command List",
+                description: "Still working on the bot but here are the working and available commands for you :D",
+                fields: [{
+                    name: "Available Commands",
+                    value: "`help` `create` `listevent`",
+                }],
+                timestamp: new Date(),
+                footer: {
+                    icon_url: "https://cdn.discordapp.com/avatars/516303284873461760/e1a7397683aed74e04b46e2be2c03913.png?size=1024",
+                    text: "© Example",
+                }            
+            }});
+        }
     },
 };
